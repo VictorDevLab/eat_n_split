2183ecd30ed8f6249b8296b0db071835ab10e045
Victor Koome <koomev786@gmail.com>
2024-06-13 08:55:08 +0300

    add form for splitting a bill

--- src/App.js
+++ src/App.js
@@ -26,7 +26,10 @@ export default function App() {
     <div className="app">
       <div className="sidebar">
         <FriendList />
+        <AddFriendForm />
+        <Button>Close</Button>
       </div>
+      <FormSplitBill />
     </div>
   );
 }
@@ -43,6 +46,10 @@ function FriendList() {
   );
 }
 
+function Button({ children }) {
+  return <button className="button">{children}</button>;
+}
+
 function Friend({ friend }) {
   return (
     <li>
@@ -50,17 +57,57 @@ function Friend({ friend }) {
       <h3 className="h3">{friend.name}</h3>
 
       {/* mutually exclusive, meaning only one of them will apply */}
-      {
-        friend.balance < 0 && <p className="red">You owe {friend.name} ${Math.abs(friend.balance)}</p>
-      }
-      {
-        friend.balance > 0 && <p className="green">{friend.name} owes you ${Math.abs(friend.balance)}</p>
-      }
-      {
-        friend.balance === 0 && <p>You and {friend.name} are equal</p>
-      }
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are equal</p>}
 
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
+  );
+}
+
+function AddFriendForm() {
+  return (
+    <form className="form-add-friend">
+      <label>Friend's name</label>
+      <input type="text"></input>
+
+      <label>Image Url</label>
+      <input type="text"></input>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with X</h2>
+      <label>Bill Value</label>
+      <input type="text"></input>
+
+      <label>Your expense</label>
+      <input type="text"></input>
+
+      <label>X's expense</label>
+      <input type="text" disabled></input>
+
+      <label>Who is paying the bill</label>
+      <select>
+        <option value="you">You</option>
+        <option value="friend">Friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
   );
 }
